2c86f511411b58c1b97bc3f7322d36955d51267d
Harshith-JP <jaihari7777@gmail.com>
2025-02-07 23:20:47 +0530

    ExceptionHandling.java

---
+++ assesment/ExceptionHandling.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class ExceptionHandling {
+    public static void processInput() {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.print("Enter a number: ");
+            double num = scanner.nextDouble();
+            System.out.println("Reciprocal: " + (1 / num));
+        } catch (Exception e) {
+            System.out.println("Invalid input or division by zero!");
+        }
+    }
+
+    public static void main(String[] args) {
+        processInput();
+    }
+}
