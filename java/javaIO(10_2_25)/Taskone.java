f000625af39a58200c7d79398e5b73c25a2ee34f
Harshith-JP <jaihari7777@gmail.com>
2025-02-10 14:14:52 +0530

    task 1

---
+++ java/javaIO(10_2_25)/Taskone.java
@@ -0,0 +1,10 @@
+class Taskone{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("this is println line 2 ");
+        System.out.print("this is print line 1 ");
+        System.out.print("this is print line 2 ");
+        System.err.println("this is err line 1 ");
+        System.err.println("this is err line 2 ");
+    }
+}
