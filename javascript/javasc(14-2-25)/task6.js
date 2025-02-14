5ec46ae9be9fb8ecfc1ccccee5740e51cd997d46
Harshith-JP <jaihari7777@gmail.com>
2025-02-14 14:28:43 +0530

    divisibilityBY12

---
+++ javascript/javasc(14-2-25)/task6.js
@@ -0,0 +1,9 @@
+function checkDivisibility(num) {
+    if (num % 12 === 0) {
+        console.log(num + " is divisible by 12.");
+    } else {
+        console.log(num + " is not divisible by 12.");
+    }
+}
+var num = 44
+checkDivisibility(num);
